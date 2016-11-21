1375ee9a0b647e750e4ac469a10930eb05b25ef5
martsukka <marianne.honkasaari@gmail.com>
2016-11-21 17:53:03 +0200

    Create messaging.py

---
+++ messaging.py
@@ -0,0 +1,17 @@
+import urllib
+import urllib2
+import pprint
+import json
+import datetime
+import time
+
+from sets import Set
+
+
+class Bot():
+
+      def send_message(self):
+            self.botid = '292156429:AAEVUD_QXjCdJmFdIwehObuoptWh2IDVvnI'
+            # roboses kuva http://imgur.com/UJ87Sky
+            self.roboses_address ='
+      
